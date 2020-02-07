929d96fb108cb3c52011610999167f6f749a5427
Alexander Lavrukhine <zanhesl@gmail.com>
2020-02-07 18:19:59 +0400

    feat: add flexible api query

--- src/js/ApiGetter/ApiGetter.js
+++ src/js/ApiGetter/ApiGetter.js
@@ -11,10 +11,25 @@ class ApiGetter extends Component {
     };
   }
 
-  getInfoFromAPI = async (page = 1) => {
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&page=${page}&api_key=${API_KEY}`;
+  getInfoFromAPI = async (rover = 'curiosity', sol = 1000, camera = 'fhaz', page = 1) => {
+    const tableInfo = [];
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&page=${page}&api_key=${API_KEY}`;
     const response = await fetch(url);
-    await console.log(await response.json());
+    const responseJson = await response.json();
+    await responseJson.photos.forEach(elem => {
+      const tempDay = {
+        imgName: elem.img_src,
+        sol: elem.sol,
+        cameraShort: elem.camera.name,
+        cameraFull: elem.camera.full_name,
+        roverName: elem.rover.name,
+        roverStatus: elem.rover.status,
+      };
+      tableInfo.push(tempDay);
+      return true;
+    });
+    this.setState({ table: tableInfo });
+    console.table(this.state.table);
   };
 
   render() {
