a5e08fdefca6f27737cdb754304c80def9218327
Fr0stmourne <46089168+Fr0stmourne@users.noreply.github.com>
2020-02-12 11:18:57 +0300

    Update sorts.js

--- src/js/sorts/sorts.js
+++ src/js/sorts/sorts.js
@@ -1,12 +1,9 @@
 const CHANGE_PLUS = 1;
-const LEAVE_ELEM = 0;
 const CHANGE_MINUS = -1;
 
 function sorts(table, key) {
   return table.sort((elemA, elemB) => {
-    if (elemA[key] > elemB[key]) return CHANGE_PLUS;
-    if (elemA[key] < elemB[key]) return CHANGE_MINUS;
-    return LEAVE_ELEM;
+    return elemA[key] > elemB[key] ? CHANGE_PLUS : CHANGE_MINUS
   });
 }
 
